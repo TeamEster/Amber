d9c197ed7fb3a60b952bd1f3706ed39d6e14833b
PONDELJAK <Karlo.PONDELJAK@amplexor.com>
2019-12-05 14:27:59 +0100

    Starting Mongo DB configuration

--- src/main/java/com/amplexor/amber/AmberApplication.java
+++ src/main/java/com/amplexor/amber/AmberApplication.java
@@ -4,28 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.amplexor.amber.model.User;
-import com.amplexor.amber.repositories.UserRepository;
-
 @SpringBootApplication
 public class AmberApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(AmberApplication.class, args);
 		
-		User ponnda = new User("Karlo", "Pondeljak", "1", 123L, 1L);
-		User ponnda2 = new User("Karlo", "Pondeljak", "2", 123L, 2L);
-		User ponnda3 = new User("Karlo", "Pondeljak", "3", 123L, 3L);
-		User ponnda4 = new User("Karlo", "Pondeljak", "4", 123L, 1L);
-		User krisko = new User("Domagoj", "Krišković", "2", 223L, 1L);
-		
-		UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
-		
-		userRepository.save(ponnda);
-		userRepository.save(ponnda2);
-		userRepository.save(ponnda3);
-		userRepository.save(ponnda4);
-		userRepository.save(krisko);
+//		User ponnda = new User("Karlo", "Pondeljak", "1", 123L, 1L);
+//		User ponnda2 = new User("Karlo", "Pondeljak", "2", 123L, 2L);
+//		User ponnda3 = new User("Karlo", "Pondeljak", "3", 123L, 3L);
+//		User ponnda4 = new User("Karlo", "Pondeljak", "4", 123L, 1L);
+//		User krisko = new User("Domagoj", "Krišković", "2", 223L, 1L);
+//		
+//		UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
+//		
+//		userRepository.save(ponnda);
+//		userRepository.save(ponnda2);
+//		userRepository.save(ponnda3);
+//		userRepository.save(ponnda4);
+//		userRepository.save(krisko);
 	}
 
 }
--- src/main/java/com/amplexor/amber/repositories/UserRepository.java
+++ src/main/java/com/amplexor/amber/repositories/UserRepository.java
@@ -2,13 +2,13 @@ package com.amplexor.amber.repositories;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.amplexor.amber.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, Long> {
 	
 	public List<User> findAll(); 
 
@@ -23,5 +23,5 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	public List<User> findByEmployeeId(String employeeId);
 	
 	public List<User> findByFirstNameOrLastNameOrEmployeeId(String firstName, String lastName, String employeeId);
-	
+
 }
