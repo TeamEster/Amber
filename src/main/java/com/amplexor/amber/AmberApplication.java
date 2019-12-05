18531b11b907ae159db689a22678b21b84ec177b
PONDELJAK <Karlo.PONDELJAK@amplexor.com>
2019-12-05 11:49:05 +0100

    Users in repo

--- src/main/java/com/amplexor/amber/AmberApplication.java
+++ src/main/java/com/amplexor/amber/AmberApplication.java
@@ -2,14 +2,24 @@ package com.amplexor.amber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.amplexor.amber.model.User;
+import com.amplexor.amber.repositories.UserRepository;
 
 @SpringBootApplication
 public class AmberApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmberApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(AmberApplication.class, args);
 		
-		//User user1 = new User("Karlo", "Pondeljak", 123L, 1L);
+		User ponnda = new User("Karlo", "Pondeljak", 123L, 1L);
+		User krisko = new User("Domagoj", "Krišković", 123L, 1L);
+		
+		UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
+		
+		userRepository.save(ponnda);
+		userRepository.save(krisko);
 	}
 
 }
