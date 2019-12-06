d8c981ad859ee6867fec2eac414e2fd525eb5b66
Domagoj Kriskovic <dkrisko3@gmail.com>
2019-12-06 16:47:11 +0100

    fixed classpath issues

--- src/main/java/com/amplexor/amber/controllers/UserController.java
+++ src/main/java/com/amplexor/amber/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amplexor.amber.dto.UserDTO;
+import com.amplexor.amber.dto.users.UserDTO;
 import com.amplexor.amber.model.User;
 import com.amplexor.amber.services.UserService;
 
--- src/main/java/com/amplexor/amber/services/UserService.java
+++ src/main/java/com/amplexor/amber/services/UserService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.amplexor.amber.dto.UserDTO;
+import com.amplexor.amber.dto.users.UserDTO;
 import com.amplexor.amber.model.Card;
 import com.amplexor.amber.model.Role;
 import com.amplexor.amber.model.User;
