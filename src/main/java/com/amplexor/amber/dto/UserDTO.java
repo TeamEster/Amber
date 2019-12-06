05dcc6f5833dc9c10587aaa5a77feab3e2c977d4
PONDELJAK <Karlo.PONDELJAK@amplexor.com>
2019-12-06 14:58:44 +0100

    DTO package

---
+++ src/main/java/com/amplexor/amber/dto/UserDTO.java
@@ -0,0 +1,34 @@
+package com.amplexor.amber.dto;
+
+public class UserDTO {
+	
+	private String employeeId;
+	
+	private String cardId;
+	
+	private String roleId;
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getCardId() {
+		return cardId;
+	}
+
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+
+	public String getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
+	}
+}
