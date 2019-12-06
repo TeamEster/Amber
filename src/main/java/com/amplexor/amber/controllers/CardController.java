85a8972bc6adc0230d39bd73072bd05b47249c1d
Domagoj Kriskovic <dkrisko3@gmail.com>
2019-12-06 12:30:14 +0100

    changed cardvalidation to request param

--- src/main/java/com/amplexor/amber/controllers/CardController.java
+++ src/main/java/com/amplexor/amber/controllers/CardController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amplexor.amber.services.CardService;
@@ -21,14 +21,13 @@ public class CardController {
 	@Autowired
 	private CardService cardService;
 	
-	@RequestMapping(value = "/validate/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> validateCard(@PathVariable("id") @NotNull String cardId) {
-		
+	@RequestMapping(value = "/validate", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> validateCard(@RequestParam("id") @NotNull String cardId) {
 		boolean valid = cardService.validateCard(cardId);
 		if (valid) {
-			return new ResponseEntity<>("Ok", HttpStatus.OK); 
+			return new ResponseEntity<>(HttpStatus.OK); 
 		} else {
-			return new ResponseEntity<>("Not found", HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
 }
