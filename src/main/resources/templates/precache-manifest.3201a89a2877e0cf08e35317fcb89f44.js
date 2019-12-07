05697c8a8a4f3d9039d43be65ab900a67e90b4a6
Robert Batušić <batusic.robert@gmail.com>
2019-12-07 14:56:47 +0100

    Updated precache-manifest paths

--- src/main/resources/templates/precache-manifest.3201a89a2877e0cf08e35317fcb89f44.js
+++ src/main/resources/templates/precache-manifest.3201a89a2877e0cf08e35317fcb89f44.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "37f862deb2c2278635481f1213e9ab5d",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "b87ea42bfd8f5ca9aba2",
-    "url": "/static/css/main.f8b876c8.chunk.css"
+    "url": "./static/css/main.f8b876c8.chunk.css"
   },
   {
     "revision": "fc689c5926eb22ed9d9d",
-    "url": "/static/js/2.09f4823f.chunk.js"
+    "url": "./static/js/2.09f4823f.chunk.js"
   },
   {
     "revision": "78ddf5a465591a03b9601ba2ddd63415",
-    "url": "/static/js/2.09f4823f.chunk.js.LICENSE"
+    "url": "./static/js/2.09f4823f.chunk.js.LICENSE"
   },
   {
     "revision": "b87ea42bfd8f5ca9aba2",
-    "url": "/static/js/main.3138bdb9.chunk.js"
+    "url": "./static/js/main.3138bdb9.chunk.js"
   },
   {
     "revision": "1349ba6f0c67bac25c2b",
-    "url": "/static/js/runtime-main.dbe160ca.js"
+    "url": "./static/js/runtime-main.dbe160ca.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "./static/media/logo.5d5d9eef.svg"
   },
   {
     "revision": "22be3d5ca1f096455acf33bea624a04c",
-    "url": "/static/media/profile2.22be3d5c.png"
+    "url": "./static/media/profile2.22be3d5c.png"
   }
 ]);
